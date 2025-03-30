478ced53fbb2e2331f289ce99459e9ab0450d6c7
Rahulstark2 <rahulstark2@gmail.com>
2025-03-30 16:55:05 +0530

    Designing the Quote Component

--- frontend/src/components/Quote.tsx
+++ frontend/src/components/Quote.tsx
@@ -1,8 +1,17 @@
 export const Quote = () => {
     return <div className="bg-slate-200 h-screen flex justify-center flex-col">
         <div className="flex justify-center">
-        <div className="max-w-md text-center bg-red-200">
-        Hi from quote component
+        <div className="max-w-lg">
+            <div className="text-3xl font-bold">
+              "The customer support I recieved was exceptional. The support team went above and beyond to
+              address my concern"
+              </div>
+              <div className="max-w-md text-xl font-semibold text-left mt-4">
+                    Julies Winfield
+                </div>
+                <div className="max-w-md text-sm font-light text-slate-400">
+                    CEO | Acme Corp
+                </div>
         </div>
         </div>
     </div>
