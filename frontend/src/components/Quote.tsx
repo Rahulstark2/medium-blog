12fc4a524e0e4aadedcd54743523e0ce58243adc
Rahulstark2 <rahulstark2@gmail.com>
2025-03-30 15:56:16 +0530

    Designing the Quote Component

--- frontend/src/components/Quote.tsx
+++ frontend/src/components/Quote.tsx
@@ -1,5 +1,9 @@
 export const Quote = () => {
-    return <div className="bg-slate-200 h-screen">
+    return <div className="bg-slate-200 h-screen flex justify-center flex-col">
+        <div className="flex justify-center">
+        <div className="max-w-md text-center bg-red-200">
         Hi from quote component
+        </div>
+        </div>
     </div>
 }
