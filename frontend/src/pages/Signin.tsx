83741edb607dea93f5fba842ef0ce714f9e85673
Rahulstark2 <rahulstark2@gmail.com>
2025-04-06 14:26:27 +0530

    update Signin.tsx

--- frontend/src/pages/Signin.tsx
+++ frontend/src/pages/Signin.tsx
@@ -1,5 +1,16 @@
+import { Auth } from "../components/Auth"
+import { Quote } from "../components/Quote"
+
 export const Signin = () => {
     return <div>
-        signin
+        <div className="grid grid-cols-2">
+
+        <div>
+            <Auth type="signin"/>
+         </div>
+         <div className="invisible lg:visible">
+       <Quote />
+       </div>
+       </div>
     </div>
 }
