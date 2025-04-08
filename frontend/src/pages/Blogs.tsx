675e06232d32df6c811ad08344f79a3b07958b32
Rahulstark2 <rahulstark2@gmail.com>
2025-04-09 00:47:13 +0530

    Updated Blogs.tsx

--- frontend/src/pages/Blogs.tsx
+++ frontend/src/pages/Blogs.tsx
@@ -1,5 +1,13 @@
+import { BlogCard } from "../components/BlogCard"
+
 export const Blogs = () => {
     return <div>
-        Blogs
+        <BlogCard
+            authorName={"rahul"}
+            title={"title of the blog"}
+            content={"content of the blog"}
+            publishedDate={"2nd Feb 2024"}
+
+        />
     </div>
 }
