ee385ed27550676d70122d464ff5ff2f93c8098b
Rahulstark2 <rahulstark2@gmail.com>
2025-04-09 20:04:50 +0530

    Updated Blog.tsx

--- frontend/src/pages/Blog.tsx
+++ frontend/src/pages/Blog.tsx
@@ -1,5 +1,12 @@
+import { useBlog } from "../hooks";
+
 export const Blog = () => {
+    const {loading, blog} = useBlog();
+    if (loading) {
+        return <div>
+            loading...
+        </div>
+    }
     return <div>
-        blog
     </div>
 }
