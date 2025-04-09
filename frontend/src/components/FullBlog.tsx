9467e53c36373e3093a3185ef5b2b391c2e74b1c
Rahulstark2 <rahulstark2@gmail.com>
2025-04-09 21:17:57 +0530

    Updated FullBlog.tsx

--- frontend/src/components/FullBlog.tsx
+++ frontend/src/components/FullBlog.tsx
@@ -1,10 +1,17 @@
-export const FullBlog = () => {
-    return <div className="grid grid-cols-12 px-10">
-        <div className="grid-cols-8">
-            hi
+import { Appbar } from "./Appbar"
+
+export const FullBlog = ({blog}: {blog: Blog}) => {
+    return <div>
+        <Appbar />
+        <div className="grid grid-cols-12 px-10 w-full">
+        <div className="bg-red-200 col-span-8">
+            <div className="text-3xl font-extrabold">
+                {title}
+            </div>
         </div>
-        <div className="grid-cols-4">
+        <div className="bg-green-200 col-span-4">
             hello
         </div>
+    </div>
     </div>
 }
