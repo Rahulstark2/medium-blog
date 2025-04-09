42af447ea4ffa17e6ac8d72337908c3470f01c7b
Rahulstark2 <rahulstark2@gmail.com>
2025-04-09 15:02:56 +0530

    Updated Blogs.tsx

--- frontend/src/pages/Blogs.tsx
+++ frontend/src/pages/Blogs.tsx
@@ -2,8 +2,22 @@ import { BlogCard } from "../components/BlogCard"
 
 export const Blogs = () => {
     return <div className="flex justify-center">
-    <div className="flex justify-center max-w-xl">
+    <div className="max-w-xl">
         <BlogCard
+            authorName={"Rahul Roy Chowdhury"}
+            title={"How an ugly single page website makes $5000 a month without affiliate marketing"}
+            content={"How an ugly single page website makes $5000 a month without affiliate marketing. How an ugly single page website makes $5000 a month without affiliate marketing."}
+            publishedDate={"2nd Feb 2024"}
+
+        />
+          <BlogCard
+            authorName={"Rahul Roy Chowdhury"}
+            title={"How an ugly single page website makes $5000 a month without affiliate marketing"}
+            content={"How an ugly single page website makes $5000 a month without affiliate marketing. How an ugly single page website makes $5000 a month without affiliate marketing."}
+            publishedDate={"2nd Feb 2024"}
+
+        />
+          <BlogCard
             authorName={"Rahul Roy Chowdhury"}
             title={"How an ugly single page website makes $5000 a month without affiliate marketing"}
             content={"How an ugly single page website makes $5000 a month without affiliate marketing. How an ugly single page website makes $5000 a month without affiliate marketing."}
