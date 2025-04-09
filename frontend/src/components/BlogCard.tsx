0d6af7c8db244de34dad06da6b474befea32ae9e
Rahulstark2 <rahulstark2@gmail.com>
2025-04-09 14:58:17 +0530

    Fixed styling in BlogCard.tsx

--- frontend/src/components/BlogCard.tsx
+++ frontend/src/components/BlogCard.tsx
@@ -13,26 +13,27 @@ export const BlogCard = ({
 }: BlogCardProps) => {
     return <div className="border border-slate-200 pb-4">
         <div className="flex">
-            <div className="flex justify-center">
             <Avatar name={authorName} />
-            </div>
-        <div className="font-extralight pl-2">{authorName}
+            
+        <div className="font-extralight pl-2 text-sm
+        flex justify-center flex-col">{authorName}
 
         </div>
         <div className="flex justify-center flex-col pl-2">
            <Circle/>
         </div>
-        <div className="pl-2 font-thin text-slate-500">
+        <div className="pl-2 font-thin text-slate-500 text-sm flex justify-center
+        flex-col">
         {publishedDate}
         </div>
     </div>
-    <div className="text-xl font-semibold">
+    <div className="text-xl font-semibold pt-2">
         {title}
     </div>
     <div className="text-md font-thin">
         {content.slice(0,100) + "..."}
     </div>
-    <div className="text-slate-500 text-sm font-thin">
+    <div className="text-slate-500 text-sm font-thin pt-4">
         {`${Math.ceil(content.length / 100)} minute(s)
         read`}
     </div>
@@ -46,7 +47,7 @@ function Circle() {
 }
 function Avatar({ name }: {name: string}) {
     return <div className="relative inline-flex items-center 
-    justify-center w-4 h-4 overflow-hidden bg-gray-600 rounded-full">
+    justify-center w-6 h-6 overflow-hidden bg-gray-600 rounded-full">
         <span className="text-xs font-extralight text-gray-600 dark:text-gray-300">
             {name[0]}
         </span>
