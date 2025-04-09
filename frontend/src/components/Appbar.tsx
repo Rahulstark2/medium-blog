e9c6053ce82b302547b7c66fbcf1ad915f55c93d
Rahulstark2 <rahulstark2@gmail.com>
2025-04-09 15:58:33 +0530

    Added Appbar.tsx

---
+++ frontend/src/components/Appbar.tsx
@@ -0,0 +1,11 @@
+export const Appbar = () => {
+    return <div className="border-b
+    flex justify-between px-10">
+        <div>
+            Medium
+        </div>
+        <div>
+            
+        </div>
+    </div>
+}
