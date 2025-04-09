398da6d7927f5c2695934cd480e75fb8fbda0e2e
Rahulstark2 <rahulstark2@gmail.com>
2025-04-09 18:23:39 +0530

    Updated Blogs.tsx

--- frontend/src/pages/Blogs.tsx
+++ frontend/src/pages/Blogs.tsx
@@ -15,7 +15,7 @@ export const Blogs = () => {
     <div className="flex justify-center">
     <div className="max-w-xl">
         {blogs.map(blog => <BlogCard
-            authorName={blog.author.name}
+            authorName={blog.author.name || "Anonymous"}
             title={blog.title}
             content={blog.content}
             publishedDate={"2nd Feb 2024"}
