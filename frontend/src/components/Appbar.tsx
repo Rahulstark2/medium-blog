07bbf926951d7d89e8e9995b0a9726ceb49d8c8c
Rahulstark2 <rahulstark2@gmail.com>
2025-04-09 17:20:27 +0530

    Updated Appbar.tsx

--- frontend/src/components/Appbar.tsx
+++ frontend/src/components/Appbar.tsx
@@ -4,11 +4,11 @@ import { Avatar } from "./BlogCard"
 export const Appbar = () => {
     return <div className="border-b
     flex justify-between px-10 py-4">
-        <div>
+        <div className="flex flex-col justify-center">
             Medium
         </div>
         <div>
-            <Avatar size={10} name="rahul" />
+            <Avatar size={"big"} name="rahul" />
         </div>
     </div>
 }
