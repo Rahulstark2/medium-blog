37464c90737916be20731383c54a7beef3c66612
Rahulstark2 <rahulstark2@gmail.com>
2025-04-09 18:18:51 +0530

    Updated Blogs.tsx

--- frontend/src/pages/Blogs.tsx
+++ frontend/src/pages/Blogs.tsx
@@ -15,9 +15,9 @@ export const Blogs = () => {
     <div className="flex justify-center">
     <div className="max-w-xl">
         {blogs.map(blog => <BlogCard
-            authorName={"Rahul Roy Chowdhury"}
-            title={"How an ugly single page website makes $5000 a month without affiliate marketing"}
-            content={"How an ugly single page website makes $5000 a month without affiliate marketing. How an ugly single page website makes $5000 a month without affiliate marketing."}
+            authorName={blog.author.name}
+            title={blog.title}
+            content={blog.content}
             publishedDate={"2nd Feb 2024"}
 
         />)}
