977540abd338ba6854f2bff8ce722f6342a687c2
Rahulstark2 <rahulstark2@gmail.com>
2025-04-11 13:15:45 +0530

    Integrated BlogSkeleton Component in Blogs.tsx

--- frontend/src/pages/Blogs.tsx
+++ frontend/src/pages/Blogs.tsx
@@ -1,5 +1,6 @@
 import { Appbar } from "../components/Appbar"
 import { BlogCard } from "../components/BlogCard"
+import { BlogSkeleton } from "../components/BlogSkeleton";
 import { useBlogs } from "../hooks";
 
 export const Blogs = () => {
@@ -7,7 +8,9 @@ export const Blogs = () => {
 
     if(loading) {
         return <div>
-            loading...
+            <BlogSkeleton />
+            <BlogSkeleton />
+            <BlogSkeleton />
         </div>
     }
     return <div>
