60960df3302df6f9a0db73708e21e661b37d2fb9
Rahulstark2 <rahulstark2@gmail.com>
2025-04-11 13:27:29 +0530

    Improved Skeleton Structure

--- frontend/src/components/BlogSkeleton.tsx
+++ frontend/src/components/BlogSkeleton.tsx
@@ -1,3 +1,5 @@
+import { Circle } from "./BlogCard"
+
 export const BlogSkeleton = () => {
     return <div role="status" className="max-w-sm animate-pulse">
         <div className="p-4 border-b border-slate-200 pb-4 w-screen max-w-screen-md cursor-pointer">
@@ -10,26 +12,19 @@ export const BlogSkeleton = () => {
                 </div>
                 <div className="pl-2 font-thin text-slate-500 text-sm flex justify-center
                 flex-col">
-                {publishedDate}
+                <div className="h-2 bg-gray-200 rounded-full mb-2.5"></div>
                 </div>
             </div>
             <div className="text-xl font-semibold pt-2">
-                {title}
+            <div className="h-2 bg-gray-200 rounded-full mb-2.5"></div>
             </div>
             <div className="text-md font-thin">
-                {content.slice(0,100) + "..."}
+            <div className="h-2 bg-gray-200 rounded-full mb-2.5"></div>
             </div>
             <div className="text-slate-500 text-sm font-thin pt-4">
-                {`${Math.ceil(content.length / 100)} minute(s)
-                read`}
+            <div className="h-2 bg-gray-200 rounded-full mb-2.5"></div>
             </div>
             </div>
-        <div className="h-2.5 bg-gray-200 rounded-full w-48 mb-4"></div>
-        <div className="h-2 bg-gray-200 rounded-full max-w-[360px] mb-2.5"></div>
-        <div className="h-2 bg-gray-200 rounded-full mb-2.5"></div>
-        <div className="h-2 bg-gray-200 rounded-full max-w-[330px] mb-2.5"></div>
-        <div className="h-2 bg-gray-200 rounded-full max-w-[300px] mb-2.5"></div>
-        <div className="h-2 bg-gray-200 rounded-full max-w-[360px]"></div>
         <span className="sr-only">Loading...</span>
     </div>
 }
